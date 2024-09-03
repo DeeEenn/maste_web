792819450ab644f268d1d7a977e14b35893b9b0e
davidnic <davi.nic@post.cz>
2024-09-03 12:59:09 +0200

    done home

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-scroll';
+
+const Header = () => {
+    return (
+        <header className="bg-green-400 text-white w-full h-12 fixed top-0 z-50 shadow-lg">
+            <div className="container mx-auto flex justify-between items-center p-4">
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      button: '#E0E5B6',
+
+    },
+    extend: {},
+    fontFamily: {
+      'cormorant': ['"Cormorant Garamond"', 'serif'],
+    },
+  },
+  plugins: [],
+}
